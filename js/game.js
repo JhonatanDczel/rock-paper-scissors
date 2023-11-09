47639aaaeb8f0a0f0a61919b148ea6a06bba9452
JhonatanDczel <jariasq@unsa.edu.pe>
2023-11-09 15:52:46 -0500

    Crea el metodo getComputerChoice
    El metodo elige una opcion basada en un numero random en el rango 1 a 3

--- js/game.js
+++ js/game.js
@@ -1,1 +1,14 @@
-console.log("Hi world!");
+function getComputerChoice() {
+  let numAzar = Math.floor(Math.random() * 3);
+  switch (numAzar) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+    default:
+      "something is not run";
+  }
+}
+
